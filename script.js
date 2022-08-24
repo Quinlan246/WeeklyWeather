5d2c754dcd24f7b3190c5a389c4a9b7bf8fb5b66
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-08-24 18:02:58 -0500

    added more functions and icons for weather

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var apiKey = "b873cc8e2dbadecf45255f14f31f8170";
 
 $(document).ready(function () {
     var searchBtn = $('#searchBtn');
@@ -6,6 +5,7 @@ $(document).ready(function () {
     searchBtn.on("click", function (e) {
         e.preventDefault();
         var cityName = $("#city").val();
+        var apiKey = "b873cc8e2dbadecf45255f14f31f8170";
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&cnt=5&units=imperial&appid=" + apiKey;
         $.ajax({
             url: queryURL,
@@ -18,6 +18,38 @@ $(document).ready(function () {
     });
 })
 
+function weatherForecast(results) {
+    $(".hide").attr("class", "row");
+    var currentCityName = results.name;
+    $("#currentCityInfo").text(currentCityName + " ");
+    var currentCityLon = results.coord.lon;
+    var currentCityLat = results.coord.lat;
+    findWithCoords(currentCityLat, currentCityLon);
+    var currentCityDate = results.sys.sunrise;
+    dateConverter(currentCityDate);
+    var currentWeatherIcon = results.weather[0].icon;
+    weatherIcon(currentWeatherIcon);
+};
+
+function findWithCoords(currentCityCoLat, currentCityCoLon) {
+    var apiKey = "b873cc8e2dbadecf45255f14f31f8170";
+    var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?lat=" + currentCityCoLat + "&lon=" + currentCityCoLon + "&exclude=minutely,hourly&units=imperial&appid=" + apiKey;
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    }).then(function (results) {
+        var currentCityTemp = results.current.temp;
+        $("#currentTemp").text("Temperature: " + currentCityTemp + " \u00B0F");
+        var currentCityHumidity = results.current.humidity;
+        $("#currentHumid").text("Humidity: " + currentCityHumidity + "%");
+        var currentCityWindSpeed = results.current.wind_speed;
+        $("#currentWind").text("Wind Speed: " + currentCityWindSpeed + " MPH");
+        var currentCityUvi = results.current.uvi;
+        uviIndexSeverity(currentCityUvi);
+        fiveDayForecast(results);
+    });
+};
+
 function initialzeLocalStorage () {
     if (localStorage.getItem('prevSearch') === null) {
         localStorage.setItem('prevSearch', '[]');
@@ -25,3 +57,4 @@ function initialzeLocalStorage () {
         return;
     }
 }
+
